fc264131cf7ff24b30a176f64f51df64b4bf38b5
nas <vividnasubi@gmail.com>
2025-08-08 15:33:18 +0900

    SpringBootプロジェクトの初期化

---
+++ api/src/main/java/com/koedame/bbs/api/KoedameApplication.java
@@ -0,0 +1,13 @@
+package com.koedame.bbs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoedameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoedameApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/koedame/bbs/api/KoedameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koedame.bbs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoedameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
