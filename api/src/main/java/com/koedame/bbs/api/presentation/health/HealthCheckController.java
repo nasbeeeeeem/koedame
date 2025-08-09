e8f0a28219f4543ea2abf370cd3c7637cfc53a2e
nas <vividnasubi@gmail.com>
2025-08-09 12:35:52 +0900

    疎通確認用のエンドポイントを実装

---
+++ api/src/main/java/com/koedame/bbs/api/presentation/health/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.koedame.bbs.api.presentation.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
+
